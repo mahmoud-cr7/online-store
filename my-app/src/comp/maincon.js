8a9487baeed8ca76ecbc01b710f1a89f24020b00
omarwalid72 <owalid193@gmail.com>
2022-12-17 22:38:50 +0200

    src-comp commit

---
+++ my-app/src/comp/maincon.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./maincon.css"
+
+function Maincon({pageName}) {
+    return (
+      
+      
+  
+       
+        <main>{pageName}</main>
+        
+       
+     
+    );
+  }
+  
+  export default Maincon;
