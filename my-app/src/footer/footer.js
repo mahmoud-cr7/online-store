82393157b2e4fc73e33c98f85f246a2f53e4a66d
omarwalid72 <owalid193@gmail.com>
2022-12-21 22:17:36 +0200

    footer commit

---
+++ my-app/src/footer/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./footer.css"
+function Footer() {
+    return (
+      <>
+       <footer>
+        www.africaStore.com
+        </footer>
+      </>
+    );
+  }
+  
+  export default Footer;
