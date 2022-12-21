7a3c97d95669dd4a80a418b083aa7167ba3b597f
omarwalid72 <owalid193@gmail.com>
2022-12-21 22:22:30 +0200

    another_commite

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,16 @@
 
 import './App.css';
+import Footer from './footer/footer';
+import Header from './header/header';
+
+
 
 function App() {
   return (
     <div className="App">
-      
-       
+      <Header></Header>
+     
+     <Footer></Footer>
     </div>
   );
 }
