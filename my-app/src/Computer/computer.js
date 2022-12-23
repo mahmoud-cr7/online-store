15a0226b028ec9384a550e72b76430e7e91b8659
mahmoud-cr7 <you@example.comwageeh@DESKTOP-OLGM1G0>
2022-12-23 15:00:58 +0200

    first step in computer page

---
+++ my-app/src/Computer/computer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../header/header";
+function Computer() {
+  return (
+    <div className="Computer">
+      <Header/>
+    
+            com page
+     
+    </div>
+  );
+}
+
+export default Computer;
