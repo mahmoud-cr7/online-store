86fc55132880381614482f535b4875aad76a04de
omarwalid72 <owalid193@gmail.com>
2022-12-24 22:35:40 +0200

    login and register commit

---
+++ my-app/src/Login/login.js
@@ -0,0 +1,34 @@
+import { text } from "@fortawesome/fontawesome-svg-core";
+import React from "react";
+
+import "./login.css"
+
+import "./login.css"
+export default function Login(){
+return(
+    <div className="parent">
+        <div className = "register">
+<form>
+   
+<input id ="name" type ="text" placeholder="Username....."/>
+
+
+<input id ="password" type ="password" placeholder="password....."/>
+
+
+
+ <a href="#">forget password !!</a>
+
+<div style={{textAlign:"center"}}>
+
+    <button type="submit">Register</button>
+</div>
+</form>
+</div>
+
+    </div>
+   
+)
+
+
+}
