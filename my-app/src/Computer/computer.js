81f40abf296a637f5fbc376141c2144dd4d31788
mahmoud-cr7 <you@example.comwageeh@DESKTOP-OLGM1G0>
2022-12-28 18:46:06 +0200

    update to home and comp and lap and prod css

--- my-app/src/Computer/computer.js
+++ my-app/src/Computer/computer.js
@@ -1,26 +1,101 @@
-import React from "react";
+import React, { Component, Fragment } from "react";
 import Header from "../header/header";
 import './computer.css'
-function Computer() {
-  return (
-
-    <div className="Computer">
-      <Header/>
-     <div className="h1+p">
-     <h1 className="headh1">COMPUTER</h1>
-      <p className="headp"> SECTION</p>
-      <hr className="line"/>
-     </div>
-     <div className="container">
-      <div className="card">
-        <img/>
-        <h3></h3>
-        <p></p>
-        
-      </div>
-     </div>
-    </div>
-  );
-}
+import Footer from "../footer/footer";
+import cardimg  from '../images/cardimg.jpg';
+class Computer extends Component {
+  render(){
+    return (
+      <div>
+          <div className="Computer">
+            <Header/>
+           <div className="h1p">
+           <h1 className="headh1">COMPUTER</h1>
+            <p className="headp"> SECTION</p>
+            {/* <hr className="line"/> */}
+           </div>
+           <div className="container">
+            <div className="card">
+             <div className="abear">
+             <img src={cardimg}/>
+              <button className="add">Add to cart</button>
+             </div>
+              <h3 className="h3">Mac book</h3>
+              <p> This Store About Computers And its Accessories Lorem ipsum dolor sit
+                <br/>
+                amet consectetur adipisicing elit. Dolor laudantium vitae dolore</p>
+              <div className="specs">
+              <div className="price">price: $10000</div>
+              <div className="sold">sold out</div>
+              <div className="quantatiy"> the quantatiy : 20</div>
+             
+              </div>
+            </div>
+            <div className="card">
+             <div className="abear">
+             <img src={cardimg}/>
+              <button className="add">Add to cart</button>
+             </div>
+              <h3 className="h3">Mac book</h3>
+              <p> This Store About Computers And its Accessories Lorem ipsum dolor sit
+                <br/>
+                amet consectetur adipisicing elit. Dolor laudantium vitae dolore</p>
+              <div className="specs">
+              <div className="price">price: $10000</div>
+              <div className="sold">sold out</div>
+              <div className="quantatiy"> the quantatiy : 20</div>
+             
+              </div>
+            </div>
+            <div className="card">
+             <div className="abear">
+             <img src={cardimg}/>
+              <button className="add">Add to cart</button>
+             </div>
+              <h3 className="h3">Mac book</h3>
+              <p> This Store About Computers And its Accessories Lorem ipsum dolor sit
+                <br/>
+                amet consectetur adipisicing elit. Dolor laudantium vitae dolore</p>
+              <div className="specs">
+              <div className="price">price: $10000</div>
+              <div className="sold">sold out</div>
+              <div className="quantatiy"> the quantatiy : 20</div>
+             
+              </div>
+            </div>
+            <div className="card">
+              <img src={cardimg}/>
+              <h3 className="h3">Mac book</h3>
+              <p> This Store About Computers And its Accessories Lorem ipsum dolor sit
+                <br/>
+                amet consectetur adipisicing elit. Dolor laudantium vitae dolore</p>
+              <div className="specs">
+              <div className="price">price: $10000</div>
+              <div className="sold">sold out</div>
+              <div className="quantatiy"> the quantatiy : 20</div>
+              </div>
+            </div>
+            <div className="card">
+              <img src={cardimg}/>
+              <h3 className="h3">Mac book</h3>
+              <p> This Store About Computers And its Accessories Lorem ipsum dolor sit
+                <br/>
+                amet consectetur adipisicing elit. Dolor laudantium vitae dolore</p>
+              <div className="specs">
+              <div className="price">price: $10000</div>
+              <div className="sold">sold out</div>
+              <div className="quantatiy"> the quantatiy : 20</div>
+              </div>
+            </div>
+           </div>
+          
+      
+          </div>
+          <Footer/>
+          </div>
+        );
+      }
+  }
+  
 
 export default Computer;
--- my-app/src/Laptop/lap.js
+++ my-app/src/Laptop/lap.js
@@ -1,18 +1,70 @@
-import React from "react";
+import React , {Component} from "react";
 import Header from "../header/header";
-import './lap.css'
-function Laptop() {
-  return (
+// import './lap.css'
+import './lap.css';
+import cardimg  from '../images/macbg.jpg';
+import Footer from "../footer/footer";
 
-    <div className="Laptop">
-      <Header/>
-     <div className="h1+p">
-     <h1 className="headh1">LAPTOPS</h1>
-      <p className="headp"> SECTION</p>
-      <hr className="line"/>
-     </div>
-    </div>
-  );
-}
+class Laptop extends Component {
+  render(){
+    return (
+        <div>
+      <div className="Laptop">
+        <Header/>
+        <div className="h1p">
+       <h1 className="headh1">LAPTOPS</h1>
+        <p className="headp"> SECTION</p>
+        {/* <hr className="line"/> */}
+       </div>
+       <div className="container">
+        <div className="card">
+         <div className="abear">
+         <img src={cardimg}/>
+          <button className="add">Add to cart</button>
+         </div>
+          <h3 className="h3">Mac book pro</h3>
+          <p> This Store About Computers And its Accessories Lorem ipsum dolor sit
+            <br/>
+            amet consectetur adipisicing elit. Dolor laudantium vitae dolore</p>
+          <div className="specs">
+          <div className="price">price: $10000</div>
+          <div className="sold">sold out</div>
+          <div className="quantatiy"> the quantatiy : 20</div>
+         
+          </div>
+        </div>
+        <div className="card">
+          <img src={cardimg}/>
+          <h3 className="h3">Mac book</h3>
+          <p> This Store About Computers And its Accessories Lorem ipsum dolor sit
+            <br/>
+            amet consectetur adipisicing elit. Dolor laudantium vitae dolore</p>
+          <div className="specs">
+          <div className="price">price: $10000</div>
+          <div className="sold">sold out</div>
+          <div className="quantatiy"> the quantatiy : 20</div>
+          </div>
+        </div>
+        <div className="card">
+          <img src={cardimg}/>
+          <h3 className="h3">Mac book</h3>
+          <p> This Store About Computers And its Accessories Lorem ipsum dolor sit
+            <br/>
+            amet consectetur adipisicing elit. Dolor laudantium vitae dolore</p>
+          <div className="specs">
+          <div className="price">price: $10000</div>
+          <div className="sold">sold out</div>
+          <div className="quantatiy"> the quantatiy : 20</div>
+          </div>
+        </div>
+       </div>
+     
+      </div>
+      <Footer/>
+      </div>
+    );
+  }
+  }
+ 
 
 export default Laptop;
