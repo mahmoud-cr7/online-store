8b89a4158755680838eed80dee252173fd025263
mahmoud-cr7 <you@example.comwageeh@DESKTOP-OLGM1G0>
2022-12-28 18:55:34 +0200

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,26 @@
 import './fontawesome'
 import Home from './Home/home';
-import './App.css';
-import {BrowserRouter , Route} from 'react-router-dom'
-import { Component } from 'react';
+import {BrowserRouter ,Routes, Route} from 'react-router-dom'
+import React , { Component } from 'react';
 import Computer from './Computer/computer';
+import Header from './header/header';
 // import Computer from './Computer/computer';
-
+import Laptop from './Laptop/lap'
 class App extends Component {
   render(){
     return (
-      <div className="App">
-        
-        <BrowserRouter>
-        <Home/>
-        {/* <Computer/> */}
-        {/* <Route exact path='' Component ={Home} />
-        <Route exact path='/computer' Component ={Computer} /> */}
-        </BrowserRouter>
-        
+      <BrowserRouter>
+      <div className='App'>
        
-      </div>
+          {/* <Laptop/> */}
+          <Computer/>
+          {/* <Header/> */}
+          {/* <Home/> */}
+        </div>
+      </BrowserRouter>
+
+       
+    
     );
   }
   }
